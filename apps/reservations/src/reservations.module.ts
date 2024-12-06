5f282c9696c76f008da0dd335ad744435b19aede
Shreshth verma <verma2007s@gmail.com>
2024-12-06 22:14:56 +0530

    feat: added passport

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -4,6 +4,8 @@ import { ReservationsController } from './reservations.controller';
 import { DatabaseModule, LoggerModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
@@ -11,7 +13,13 @@ import { ReservationDocument, ReservationSchema } from './models/reservation.sch
     DatabaseModule.forFeature([
       { name: ReservationDocument.name, schema: ReservationSchema  }
     ]),
-    LoggerModule
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      })
+    })
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
